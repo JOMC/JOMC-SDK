4ba6c9c3a3fbba3ef303a1db48c76ff51cb1d48c
Christian Schulte <cs@schulte.it>
2009-10-08 01:09:08 +0000

    [maven-scm-plugin] Build 2009-10-08T01:08:28+0000
    git-svn-id: http://svn.jomc.org/svnroot/jomc/jomc-sdk/trunk@812 aee2edba-5628-49ba-b8b8-0e9d4ac63d57

--- src/main/java/org/jomc/sdk/naming/NamingFramework.java
+++ src/main/java/org/jomc/sdk/naming/NamingFramework.java
@@ -51,7 +51,7 @@ import javax.naming.NamingException;
 // SECTION-END
 // SECTION-START[Annotations]
 @javax.annotation.Generated( value = "org.jomc.tools.JavaSources",
-                             comments = "See http://jomc.sourceforge.net/jomc/1.0-alpha-5/jomc-tools" )
+                             comments = "See http://jomc.sourceforge.net/jomc/1.0-alpha-6/jomc-tools" )
 // SECTION-END
 public class NamingFramework
 {
@@ -67,7 +67,7 @@ public class NamingFramework
 
     /** Creates a new {@code NamingFramework} instance. */
     @javax.annotation.Generated( value = "org.jomc.tools.JavaSources",
-                                 comments = "See http://jomc.sourceforge.net/jomc/1.0-alpha-5/jomc-tools" )
+                                 comments = "See http://jomc.sourceforge.net/jomc/1.0-alpha-6/jomc-tools" )
     public NamingFramework()
     {
         // SECTION-START[Default Constructor]
