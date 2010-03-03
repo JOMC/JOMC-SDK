a10195d517e77d8385cd242ac9dcc8c2773668d4
Christian Schulte <cs@schulte.it>
2010-03-03 22:19:28 +0000

    o s/cs@jomc.org/schulte2005@users.sourceforge.net/g
    git-svn-id: http://svn.jomc.org/svnroot/jomc/jomc-sdk/trunk@1546 aee2edba-5628-49ba-b8b8-0e9d4ac63d57

--- jomc-sdk-model/src/main/java/org/jomc/sdk/model/support/JaxbMarshallerFactory.java
+++ jomc-sdk-model/src/main/java/org/jomc/sdk/model/support/JaxbMarshallerFactory.java
@@ -2,7 +2,7 @@
 // <editor-fold defaultstate="collapsed" desc=" Generated License ">
 /*
  *   Copyright (c) 2010 The JOMC Project
- *   Copyright (c) 2005 Christian Schulte <cs@jomc.org>
+ *   Copyright (c) 2005 Christian Schulte <schulte2005@users.sourceforge.net>
  *   All rights reserved.
  *
  *   Redistribution and use in source and binary forms, with or without
@@ -85,7 +85,7 @@ import static org.jomc.sdk.model.support.SdkModelProcessor.XML_SCHEMA_JAVA_CONTE
  * Dependency on {@code javax.xml.validation.Schema} bound to an instance.</blockquote></li>
  * </ul></p>
  *
- * @author <a href="mailto:cs@jomc.org">Christian Schulte</a> 1.0
+ * @author <a href="mailto:schulte2005@users.sourceforge.net">Christian Schulte</a> 1.0
  * @version $Id$
  */
 // </editor-fold>
