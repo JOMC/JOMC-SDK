b638f9be11eee68904a8cf15c87e1ea0c5a58956
Christian Schulte <cs@schulte.it>
2010-04-03 04:59:51 +0000

    o Prepared for 1.0-beta-1.
    git-svn-id: http://svn.jomc.org/svnroot/jomc/jomc-sdk/trunk@1736 aee2edba-5628-49ba-b8b8-0e9d4ac63d57

--- jomc-sdk-model/src/main/java/org/jomc/sdk/model/support/JaxpSchemaFactory.java
+++ jomc-sdk-model/src/main/java/org/jomc/sdk/model/support/JaxpSchemaFactory.java
@@ -71,7 +71,7 @@ import static org.jomc.sdk.model.support.SdkModelProcessor.XML_SCHEMA_JAVA_CLASS
 // SECTION-START[Annotations]
 // <editor-fold defaultstate="collapsed" desc=" Generated Annotations ">
 @javax.annotation.Generated( value = "org.jomc.tools.SourceFileProcessor",
-                             comments = "See http://jomc.sourceforge.net/jomc/1.0-alpha-19/jomc-tools" )
+                             comments = "See http://jomc.sourceforge.net/jomc/1.0-beta-1/jomc-tools" )
 // </editor-fold>
 // SECTION-END
 public class JaxpSchemaFactory
@@ -126,7 +126,7 @@ public class JaxpSchemaFactory
 
     /** Creates a new {@code JaxpSchemaFactory} instance. */
     @javax.annotation.Generated( value = "org.jomc.tools.SourceFileProcessor",
-                                 comments = "See http://jomc.sourceforge.net/jomc/1.0-alpha-19/jomc-tools" )
+                                 comments = "See http://jomc.sourceforge.net/jomc/1.0-beta-1/jomc-tools" )
     public JaxpSchemaFactory()
     {
         // SECTION-START[Default Constructor]
@@ -146,7 +146,7 @@ public class JaxpSchemaFactory
      * @throws org.jomc.ObjectManagementException if getting the property instance fails.
      */
     @javax.annotation.Generated( value = "org.jomc.tools.SourceFileProcessor",
-                                 comments = "See http://jomc.sourceforge.net/jomc/1.0-alpha-19/jomc-tools" )
+                                 comments = "See http://jomc.sourceforge.net/jomc/1.0-beta-1/jomc-tools" )
     private org.jomc.sdk.model.SchemasType getSchemas()
     {
         final org.jomc.sdk.model.SchemasType _p = (org.jomc.sdk.model.SchemasType) org.jomc.ObjectManagerFactory.getObjectManager( this.getClass().getClassLoader() ).getProperty( this, "schemas" );
