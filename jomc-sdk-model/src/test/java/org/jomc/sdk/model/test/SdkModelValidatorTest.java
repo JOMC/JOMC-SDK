4962b81349be0110ac8c5325cec1987ff1cb5940
Christian Schulte <cs@schulte.it>
2010-06-02 19:15:17 +0000

    [maven-scm-plugin] Build 2010-06-02T21:11:12+0200
    git-svn-id: http://svn.jomc.org/svnroot/jomc/jomc-sdk/trunk@1963 aee2edba-5628-49ba-b8b8-0e9d4ac63d57

--- jomc-sdk-model/src/test/java/org/jomc/sdk/model/test/SdkModelValidatorTest.java
+++ jomc-sdk-model/src/test/java/org/jomc/sdk/model/test/SdkModelValidatorTest.java
@@ -58,7 +58,7 @@ import static org.junit.Assert.assertFalse;
 // SECTION-END
 // SECTION-START[Annotations]
 // <editor-fold defaultstate="collapsed" desc=" Generated Annotations ">
-@javax.annotation.Generated( value = "org.jomc.tools.SourceFileProcessor", comments = "See http://jomc.sourceforge.net/jomc/1.0-beta-4-SNAPSHOT/jomc-tools" )
+@javax.annotation.Generated( value = "org.jomc.tools.SourceFileProcessor 1.0-beta-4-SNAPSHOT", comments = "See http://jomc.sourceforge.net/jomc/1.0-beta-4-SNAPSHOT/jomc-tools" )
 // </editor-fold>
 // SECTION-END
 public class SdkModelValidatorTest
@@ -135,7 +135,7 @@ public class SdkModelValidatorTest
     // <editor-fold defaultstate="collapsed" desc=" Generated Constructors ">
 
     /** Creates a new {@code SdkModelValidatorTest} instance. */
-    @javax.annotation.Generated( value = "org.jomc.tools.SourceFileProcessor", comments = "See http://jomc.sourceforge.net/jomc/1.0-beta-4-SNAPSHOT/jomc-tools" )
+    @javax.annotation.Generated( value = "org.jomc.tools.SourceFileProcessor 1.0-beta-4-SNAPSHOT", comments = "See http://jomc.sourceforge.net/jomc/1.0-beta-4-SNAPSHOT/jomc-tools" )
     public SdkModelValidatorTest()
     {
         // SECTION-START[Default Constructor]
